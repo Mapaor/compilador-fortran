cf998a2080c32e3a37cf37fb0b53acc721f8972d
Martí <135515260+Mapaor@users.noreply.github.com>
2024-09-28 23:04:15 +0200

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -x  # Debugging mode to print executed commands
 echo "INSTALLING FORTRAN..."
-sleep 1  # Give it a second to ensure output is flushed
 apt update && apt install -y gfortran
 
