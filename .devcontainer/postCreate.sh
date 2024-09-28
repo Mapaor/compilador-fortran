1b659d3cd2f03b5cf62ab57f7ec0ea1c64709024
Martí <135515260+Mapaor@users.noreply.github.com>
2024-09-28 22:48:24 +0200

    Update postCreate.sh

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "INSTAL.LANT FORTRAN..."
+set -x  # Enable debug mode to print each command being executed
+echo "INSTALLING FORTRAN..."
 sudo apt update && sudo apt install -y gfortran
